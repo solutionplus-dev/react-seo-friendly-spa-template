cc55b134530ef740cf1f3dbb501e0c9ef227e8e1
Saif <saif@aghazinvest.com>
2022-01-24 12:15:18 +0500

    minor Ui fix

--- src/components/Resume/Components/Resume.js
+++ src/components/Resume/Components/Resume.js
@@ -32,12 +32,19 @@ class Resume extends Component {
 
     return (
       <section id="resume">
-        <div className="row education">
-          <h1><span>Education</span></h1>
-          <div className="twelve columns">
-            {education}
-          </div>
-        </div>
+         <div className="row education">
+         <div className="three columns header-col">
+            <h1><span>Education</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+            <div className="row item">
+               <div className="twelve columns">
+                 {education}
+               </div>
+            </div>
+         </div>
+      </div>
       <div className="row work">
 
          <div className="three columns header-col">
